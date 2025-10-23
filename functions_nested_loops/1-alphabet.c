059bbbd710763779aa8fe2c0d630571068be2566
12190-cpu <12190@holbertonstudents.com>
2025-10-23 12:02:10 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	_putchar(c);
+
+	_putchar('\n');
+}
+{
+	return (0);
+}
