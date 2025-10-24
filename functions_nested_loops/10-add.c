4160e22cb74edc65be7fe6bad26b7c063fd81feb
12190-cpu <12190@holbertonstudents.com>
2025-10-24 16:30:38 +0000

    add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _add - adds 2 int
+ * add - adds 2 int
  *@x: first int
  *@y: second int
  *
  */
 
-int _add(int x, int y);
+int add(int x, int y)
 {
 	return (x + y);
 }
