03be5fe68989944e5dcb459c944a4a22674076f1
12190-cpu <12190@holbertonstudents.com>
2025-10-24 08:47:45 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -2,17 +2,26 @@
 
 /**
  * print_sign - print the sign of a number
- * @'n' - number
+ * @n - the number to check
  *
- * Return: Always 0 (Success)
+ * Return: 1 or 0 or -1
  */
 
 int print_sign(int n)
 {
 	if (n > 0)
+	{
+		_putchar ('+');
 		return (1);
+	}
 	else if (n == 0)
+	{
+		_putchar ('0')
 		return (0);
+	}
 	else
+	{
+		_putchar ('-')
 		return (-1);
+	}
 }
