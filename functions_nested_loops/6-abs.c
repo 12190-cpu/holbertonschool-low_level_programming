abfd6c3535e9d3bb7a96c822deaa1486a32a6c83
12190-cpu <12190@holbertonstudents.com>
2025-10-24 09:58:59 +0000

    abs

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * abs - print the absolute value of a integer
+ * @int: the integer
+ *
+ * Return: 1 or 0 or -1
+ */
+
+int _abs(int)
+{
+	int i;
+	printf("%d\n", i);
+
+	return (0);
+}
