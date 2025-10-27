e3bbc2b99071865c85fc02981a53d565f0d71307
12190-cpu <12190@holbertonstudents.com>
2025-10-27 13:14:59 +0000

    mul

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two int
+ *@a: first int
+ *@b: second int
+ *
+ * Return: Always 0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	printf("Multiplication: %d\\n", (a * b);
+			}
