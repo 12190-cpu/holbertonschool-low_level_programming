3cd7eaacb39d5dee91c68071112efd681c7b3ccf
12190-cpu <12190@holbertonstudents.com>
2025-10-27 13:45:15 +0000

    num

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints the numbers
+ *@i: the int to run
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+	_putchar(i + '0');
+	_putchar('\n');
+}
