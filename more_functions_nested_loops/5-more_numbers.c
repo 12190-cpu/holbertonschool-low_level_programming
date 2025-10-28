feda05c3fbec2a87aadcfa0ea000cc22cda8b53c
12190-cpu <12190@holbertonstudents.com>
2025-10-28 08:50:51 +0000

    morenumbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - prints the numbers
+ *@i: the int to run
+ *@j: the imt to run
+ *
+ * Return: Always 0 (Success)
+ */
+
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if (j >= 10)
+	_putchar('1');
+	_putchar((j % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
