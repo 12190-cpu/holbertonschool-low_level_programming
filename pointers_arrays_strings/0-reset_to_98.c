68e9afe4197c103a7c0ef93e7d3a970c3eb5be18
12190-cpu <12190@holbertonstudents.com>
2025-10-29 11:40:41 +0000

    pointers

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of an int to 98
+ * @n: pointer to an int
+ *
+ * Return: void
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
