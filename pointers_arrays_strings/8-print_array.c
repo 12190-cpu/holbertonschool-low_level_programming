47d2290f78c331063c950637f549e8ca89e1be2f
12190-cpu <12190@holbertonstudents.com>
2025-10-29 18:41:59 +0000

    array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_array - affiche n éléments d'un tableau d'entiers
+ * @a: pointeur vers le tableau d'entiers
+ * @n: nombre d'élémenta afficher
+ *
+ * Retour: rien (void)
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+	printf("%d", a[i]);
+
+	if (i != n - 1)
+	printf(", ");
+	}
+	printf("\n");
+}
