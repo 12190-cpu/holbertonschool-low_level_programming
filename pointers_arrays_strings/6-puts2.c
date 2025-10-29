84e6fde69401f0410e9f7de28cf82f1d5cd96717
12190-cpu <12190@holbertonstudents.com>
2025-10-29 14:10:51 +0000

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,16 @@ void puts2(char *str)
 {
 	int i = 0;
 
+	if (!str)
+	return;
+
 	while (str[i] != '\0')
 	{
 	_putchar(str[i]);
 	i += 2;
+
+	if (str[i] == '\0')
+	break;
 	}
 
 	_putchar('\n');
