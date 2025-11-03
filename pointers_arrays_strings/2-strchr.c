5e770733b399a3dba78db68c54a93c60d964ff59
12190-cpu <12190@holbertonstudents.com>
2025-11-03 13:29:15 +0000

    memcpy

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strchr - localise un caractere dans une chaine
+ * @s: la chaine a checker
+ * @c: le caractere a localiser
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+		if (*s == c)
+			return (s);
+	return (NULL);
+}
