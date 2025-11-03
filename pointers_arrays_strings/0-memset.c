b7bc4e31de857acb94fac0461ca38a0693808bdb
12190-cpu <12190@holbertonstudents.com>
2025-11-03 11:51:23 +0000

    all

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * memset - met en memoire avec un meme octet
+ * @*s: pointeur vers le debut de la memoire
+ * @b: octet a mettre en memoire
+ *
+ *
+ * Return: La chaîne modifiée
+ */
+char *_memset(char *s, char b, unsigned int n);
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+
+	return s;
+}
