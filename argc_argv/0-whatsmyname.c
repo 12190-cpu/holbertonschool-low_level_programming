8632550722cb4043c62deaf7c54c7726e1b2fc59
12190-cpu <12190@holbertonstudents.com>
2025-11-07 13:26:24 +0000

    all

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *main - affiche le nom du prog
+ * @argc: nombre d'arguments
+ * @argv: tableau d'arguments
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
