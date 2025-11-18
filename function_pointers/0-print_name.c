b94ffd6e9f688914c580c2e967d76dbda643ffdf
12190-cpu <12190@holbertonstudents.com>
2025-11-18 11:22:47 +0000

    funcpoint

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+#include <stdion.h>
+
+/**
+ * print_name - affiche un nom
+ * @name: poitne vers le nom
+ * @f: pointe vers fonction
+ *
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
