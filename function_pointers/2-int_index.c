f5f425afd0e6791e296030ae76159fc9c06dc052
12190-cpu <12190@holbertonstudents.com>
2025-11-19 11:04:34 +0000

    funcpoint

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,28 +1,28 @@
 #include "function_pointers.h"
 
 /**
- * int_index - cherche un entier ds le tableau
- * @array: tableau
+ * int_index - cherche un entier dans un tableau
+ * @array: tableau d'entiers
  * @size: taille du tableau
- * @cmp: pointeur vers la fonction
+ * @cmp: pointeur vers la fonction de comparaison
  *
- * Return: index du 1er element ou -1 si aucun
+ * Return: index du premier élément correspondant, ou -1 si aucun
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
 	if (array == NULL || cmp == NULL)
-		return (-1)
+		return (-1);
 
 	if (size <= 0)
-		retutrn (-1);
+		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-			return (-1)
+			return (i);
 	}
 
-	return (-1)
+	return (-1);
 }
