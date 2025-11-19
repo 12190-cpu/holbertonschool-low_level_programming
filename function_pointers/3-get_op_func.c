b715ff613e5799f1df982edd62d4397287b79f8d
12190-cpu <12190@holbertonstudents.com>
2025-11-19 18:21:23 +0000

    funcpoint

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,5 +1,4 @@
 #include "3-calc.h"
-#include <string.h>
 
 /**
  * get_op_func - choisit l'operateur selon la fonction 
@@ -23,4 +22,12 @@ int (*get_op_func(char *s))(int, int)
 	{NULL, NULL}
 	};
 	int i;
+
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
 }
