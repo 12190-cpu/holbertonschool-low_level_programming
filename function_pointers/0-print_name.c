7f0e7c6ffe18b26b584c6831e485391fdada018d
12190-cpu <12190@holbertonstudents.com>
2025-11-19 09:24:55 +0000

    funcpoint

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -6,6 +6,8 @@
  * @name: pointe vers le nom
  * @f: pointe vers fonction
  *
+ * Description: Cette fonction appelle la fonction pointée par 
+ * pour afficher le nom. Si f ou name est NULL, elle ne fait rien.
  */
 void print_name(char *name, void (*f)(char *))
 {
