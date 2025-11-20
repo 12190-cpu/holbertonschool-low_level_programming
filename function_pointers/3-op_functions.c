42806f8c5c41e7a77e9c1f31cea4ce8962180b3b
12190-cpu <12190@holbertonstudents.com>
2025-11-20 10:40:35 +0000

    funcpoint

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@
  *
  * Return: resultat de l'adition
  */
-int op_add(int a, int b);
+int op_add(int a, int b)
 {
 	return (a + b);
 }
@@ -19,7 +19,7 @@ int op_add(int a, int b);
  *
  * Return: resultat de la soustraction
  */
-int op_sub(int a, int b);
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
@@ -31,7 +31,7 @@ int op_sub(int a, int b);
  *
  * Return: resultat de la multiplication
  */
-int op_mul(int a, int b);
+int op_mul(int a, int b)
 {
 	return (a * b);
 }
@@ -43,7 +43,7 @@ int op_mul(int a, int b);
  *
  * Return: resultat de la division
  */
-int op_div(int a, int b);
+int op_div(int a, int b)
 {
 	return (a / b);
 }
@@ -55,7 +55,7 @@ int op_div(int a, int b);
  *
  * Return: reste de la division
  */
-int op_mod(int a, int b);
+int op_mod(int a, int b)
 {
 	return (a % b);
 }
