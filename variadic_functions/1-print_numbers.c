8cc5989cd1fb258ce08bbc28651df239e6207a67
12190-cpu <12190@holbertonstudents.com>
2025-11-21 13:11:47 +0000

    variadic

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -11,6 +11,8 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+    unsigned int i;
+    va_list args;
 
     va_start(args, n);
 
@@ -23,6 +25,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     }
 
     va_end(args);
-    printf("\n");
 }
 
