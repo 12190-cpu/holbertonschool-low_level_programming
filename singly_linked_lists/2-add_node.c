3508f16d99b6a149287efd766621add948f7f705
12190-cpu <12190@holbertonstudents.com>
2025-12-02 10:43:34 +0000

    singly lists

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * add_node - adds a new node at the beginning of the list
+ * @head: double pointer to the head of the list
+ * @str: string to duplicate and insert
+ *
+ * Return: address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
