ba11fd1b1efe31663df08d632bd11eb9839a66d1
12190-cpu <12190@holbertonstudents.com>
2025-12-04 09:12:48 +0000

    README

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,6 +11,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
+
 	if (!newNode)
 		return (NULL);
 
--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -10,36 +10,36 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-    dlistint_t *newNode, *temp = *h;
-    unsigned int i = 0;
+	dlistint_t *newNode, *temp = *h;
+	unsigned int i = 0;
 
-    if (idx == 0)
-        return (add_dnodeint(h, n));
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 
-    newNode = malloc(sizeof(dlistint_t));
-    if (!newNode)
-        return (NULL);
+	newNode = malloc(sizeof(dlistint_t));
+	if (!newNode)
+		return (NULL);
 
-    while (temp != NULL && i < idx - 1)
-    {
-        temp = temp->next;
-        i++;
-    }
+	while (temp != NULL && i < idx - 1)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-    if (temp == NULL)
-    {
-        free(newNode);
-        return (NULL);
-    }
+	if (temp == NULL)
+	{
+		free(newNode);
+		return (NULL);
+	}
 
-    newNode->n = n;
-    newNode->next = temp->next;
-    newNode->prev = temp;
+	newNode->n = n;
+	newNode->next = temp->next;
+	newNode->prev = temp;
 
-    if (temp->next)
-        temp->next->prev = newNode;
+	if (temp->next)
+		temp->next->prev = newNode;
 
-    temp->next = newNode;
+	temp->next = newNode;
 
-    return (newNode);
+	return (newNode);
 }
