b1c5b06c1784d0e8e51c63ea896020c8132dc46c
12190-cpu <12190@holbertonstudents.com>
2025-12-07 16:51:46 +0000

    hash

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates an hash table
+ * @size: size of the array
+ *
+ * Return: Pointer to the created table or NULL if fail
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
+}
+
