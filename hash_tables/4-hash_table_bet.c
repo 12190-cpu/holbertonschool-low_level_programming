15ee0c751ce68a8bcd4b36b5486740f735d1d437
12190-cpu <12190@holbertonstudents.com>
2025-12-07 17:45:18 +0000

    hash

---
+++ hash_tables/4-hash_table_bet.c
@@ -0,0 +1,22 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - gets a value associated with a key
+ * @ht: hash table
+ * @key: looked for key
+ *
+ * Return: found value or NULL if missing key
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *node;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	return (NULL);
+}
