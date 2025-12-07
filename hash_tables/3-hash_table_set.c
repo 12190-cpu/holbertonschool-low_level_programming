0a777d1cd044012fa7bf172b46fcd97e127e53fe
12190-cpu <12190@holbertonstudents.com>
2025-12-07 17:26:57 +0000

    hash

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -29,3 +29,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 		temp = temp->next;
 	}
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index]; /* Chaînage (collision) */
+	ht->array[index] = new_node;
+}
