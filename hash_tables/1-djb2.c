bb5b01a0d12fd16740178b0cbb69b14c30fd6bbc
12190-cpu <12190@holbertonstudents.com>
2025-12-07 17:07:17 +0000

    hash

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Impelments DJB2 algorithm
+ * @str: the chain to hash
+ *
+ * Return: the generated hash
+ */
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
